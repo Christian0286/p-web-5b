be5ac5f836e090b0da6136a50b649f276a88994b
Christian0286 <kevincruzflores4@gmail.com>
2023-02-02 10:47:24 -0600

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { componenteComponent} from './components/componente/componente.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    componenteComponent,
+    NavbarComponent,
+    FooterComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/componente/componente.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-componente',
+    templateUrl:'./componente.Component.html',
+    styleUrls: ['./componente.component.css'],
+})
+
+export class componenteComponent{
+
+}
